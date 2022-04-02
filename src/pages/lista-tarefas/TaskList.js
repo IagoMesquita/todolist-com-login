0c43347882247e3967ad24b0c56dca4897613c3e
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-04-01 21:00:18 -0300

    Logica de marca tarefa feita e excluir uma só.

--- src/pages/lista-tarefas/TaskList.js
+++ src/pages/lista-tarefas/TaskList.js
@@ -1,21 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import "./TaskList.css";
 
 function TaskList() {
-  const [task, setTasks] = useState({ task: "" });
-	console.log('task', task);
+  const [id, setId] = useState(0);
+
+  const [task, setTasks] = useState({id: id, task: ""  });
 	const [listTask, setListTask] = useState([]);
-	console.log('listTask', listTask);
 
 
 	const handleInputChange = ({ target }) => {
     const { name, value } = target;
-    setTasks({ [name]: value });
+    setTasks({ id, [name]: value });
   };
 
   const addTask = () => {
+    setId(id + 1);
     setListTask([...listTask, task]);
-    setTasks({ task: "" });
+      setTasks({ task: "" });
+
   };
 
 	const clearTasks = () => {
@@ -23,43 +25,76 @@ function TaskList() {
 	};
 
   const excludTask = ({ target }) => {
-    const { name } = target;
-    const currentTasks = listTask.filter((task) =>  task.task !== name);
-    console.log('TAks diferentes', currentTasks);
+    const { id } = target;
+    const currentTasks = listTask.filter((task) =>  task.id !== Number(id));
     setListTask([...currentTasks]);
   };
 
+
+  const doneTask = ({ target }) => {
+    const { id } = target;
+    const TaskDone = listTask.map((task) => { 
+      console.log(task.id, id)
+      return  task.id === Number(id) ? {...task, style: !task.style} : task;
+    })
+    setListTask([...TaskDone])
+  };
+
+
+
   return (
-    <div>
-      <div className="header_task">
+    <div id="content-all">
+      <header className="header_task">
        <h2>Lista de Tarefas</h2>
-      </div>
-      <div area-buttons>
-        <input
-          name="task"
-          value={task.task}
-          onChange={handleInputChange}
-          type="text"
-        />
-        <button onClick={addTask}>Adicionar</button>
-        <button>Concluida</button>
-        <button onClick={ clearTasks }>Limpar</button>
-      </div>
-      <div id="area-tarefas">
-        <div>
-          <ul>
-            {listTask.map((task) => (
-              <>
-              <li key={ task.task }>{ task.task }
-                <button name={ task.task } onClick={ excludTask } key={ task.task }>Excluir</button>
-              </li>
-              </>
-            ))}
-          </ul>
+      </header>
+      <main id="content">
+        <div area-buttons>
+          <input
+            name="task"
+            value={task.task}
+            onChange={handleInputChange}
+            type="text"
+          />
+          <button disabled={!task.task && true} onClick={addTask} >Adicionar</button>
+          <button onClick={ clearTasks }>Limpar</button>
         </div>
+        <div id="area-tarefas">
+          <div>
+            <ul>
+              {listTask.map((task) => (
+                <>
+               <li 
+                className={task.style && 'riscado'}
+                key={ task.task }
+                id={ task.id }
+                
+               >
+                  { task.task }
+                  <button 
+                    name={ task.task } 
+                    onClick={ excludTask }
+                    id={ task.id }
+                  >
+                  Excluir
+                  </button>
+                  <button 
+                    name={ task.task }
+                    onClick={ doneTask }  
+                    id={ task.id }
+                  >
+                  OK
+                  </button>
+                </li>
+                </>
+              ))}
+            </ul>
+          </div>
       </div>
+      </main>
+
     </div>
   );
+
 }
 
 export default TaskList;
